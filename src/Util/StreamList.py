0b8906074c6302d53dc793d64dcca3553bd788bd
breillyl <56320708+breillyl@users.noreply.github.com>
2019-11-11 19:29:07 -0800

    Added pagination for more complete results (Task 1)
    Updated the code with more comments and added pagination to retrieve more than the first 100 results

--- src/Util/StreamList.py
+++ src/Util/StreamList.py
@@ -1,5 +1,8 @@
 import json
 import requests
+import re
+
+## to use -> replace [Client-ID] with your client id.
 
 ## Parameters to use when your Query the API
 ## Headers include your client ID / Oauth
@@ -9,26 +12,43 @@ headers = {
     'Client-ID': '[Client-ID]',
 }
 params = (
-('name', "Fortnite"),
+    ('name', "World of Warcraft"),
 )
+channels = {}
 
 ## Gets the ID of the game you're looking for by the title of the game
+## Sends a API request to twitch to get the game ID by passing in game name as a parameter
 response = requests.get('https://api.twitch.tv/helix/games', headers=headers, params=params)
 games = json.loads(response.text)
 print("Game Title: ", (games['data'][0]['name']))
-gameID = games['data'][0]['id'];
+gameID = games['data'][0]['id']
 
-## Lists the top 100 live streamers in that game category
+
+## Lists all of the channels in a directory (first loop to get the cursor to use for pagination)
+## Gets a list of the first 100 streamers in a category then returns a cursor, which you use as a parameter in following
+## requests to get the next 100 results, until there are no results remaining
 response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=(('game_id', gameID),('first', 100),))
-streamersList = json.loads(response.text)
-streamersList = streamersList['data']
-
-##Prints the usernames, viewcount and titles of the top 100 streams in a category
-print("Top 100 live streams: ")
+streamData = json.loads(response.text)
+cursor = streamData['pagination']['cursor']
+streamersList = streamData['data']
 for streamer in streamersList:
-    print("Name:", streamer['user_name'].ljust(16), "Viewers: ",  streamer['viewer_count'],)
+    channels[streamer['user_name']] = streamer['title']
 
-##Prints a list of the top 100 streamers of the category (For easy implementation into channel text file)
-print("\n")
-for streamer in streamersList:
-    print(streamer['user_name'])
+
+## Loops through to get all of the channels (using pagination) until no channels remain
+while (cursor != -1):
+    response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=(('game_id', gameID),('first', 100), ('after', cursor),))
+    streamData = json.loads(response.text)
+    if (len(streamData['pagination']) > 0):
+         cursor = streamData['pagination']['cursor']
+    else:
+        cursor = -1
+    streamersList = streamData['data']
+    for streamer in streamersList:
+        channels[streamer['user_name']] = streamer['title']
+
+## Prints the usernames of all live channels in the category
+for x in channels:
+    print(x)
+
+print(len(channels), "channels")
