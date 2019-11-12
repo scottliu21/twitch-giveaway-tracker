566f9c4e7c2cd5b463f083ff0262019d087c305a
breillyl <56320708+breillyl@users.noreply.github.com>
2019-11-11 19:59:19 -0800

    Added Pagination to title analysis (Task 4)
    Added pagination for more accurate results

--- src/Util/TitleList.py
+++ src/Util/TitleList.py
@@ -2,6 +2,8 @@ import json
 import requests
 import re
 
+## To use -> Replace [Client-ID] with your client id and "fortnite" with any category title.
+
 ## Parameters to use when your Query the API
 ## Headers include your client ID / Oauth
 ## Parameters include name of game or other strings as required by the API
@@ -10,22 +12,43 @@ headers = {
     'Client-ID': '[Client-ID]',
 }
 params = (
-('name', "Fortnite"),
+    ('name', "Fortnite"),
 )
+channels = {}
+count = 0
 
 ## Gets the ID of the game you're looking for by the title of the game
 response = requests.get('https://api.twitch.tv/helix/games', headers=headers, params=params)
 games = json.loads(response.text)
 print("Game Title: ", (games['data'][0]['name']))
-gameID = games['data'][0]['id'];
+gameID = games['data'][0]['id']
 
-## Gets a list of the top 100 live streamers in that game category
+## Lists the top 100 live streamers in that game category
 response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=(('game_id', gameID),('first', 100),))
-streamersList = json.loads(response.text)
-streamersList = streamersList['data']
-
-## Prints a list of streamers with giveaway in their title.
-print("\nGiveaway Listings: ")
+streamData = json.loads(response.text)
+cursor = streamData['pagination']['cursor']
+streamersList = streamData['data']
 for streamer in streamersList:
-    if re.search("[Gg][Ii][Vv]", streamer['title']):
-        print(streamer['user_name'].ljust(16), "\n   Viewers: ", streamer['viewer_count'], "\n   Title: ", streamer['title'])
+    channels[streamer['user_name']] = streamer['title'].strip()
+
+## Loops through to get all of the channels (using pagination) until no channels remain
+while (cursor != -1):
+    response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=(('game_id', gameID),('first', 100), ('after', cursor),))
+    streamData = json.loads(response.text)
+    if (len(streamData['pagination']) > 0):
+         cursor = streamData['pagination']['cursor']
+    else:
+        cursor = -1
+    streamersList = streamData['data']
+    for streamer in streamersList:
+        channels[streamer['user_name']] = streamer['title'].strip()
+
+## Looks through the titles of the streams and prints a list of channels with giveaway in the title
+for x in channels:
+    if re.search("[Gg][Ii][Vv]", channels[x]):
+        print(x)
+        count +=1
+
+## Prints the total number of channels, and the number of channels with giveaway in their title
+print("\n",len(channels), "channels")
+print("", count, "giveaways")
