219b78b7cdf59126e0628389ab8ba9ac41200d79
breillyl <56320708+breillyl@users.noreply.github.com>
2019-11-15 13:17:11 -0800

    Added functions for easy implementation (Task 1)

--- src/Util/StreamList.py
+++ src/Util/StreamList.py
@@ -1,54 +1,59 @@
 import json
 import requests
-import re
 
-## to use -> replace [Client-ID] with your client id.
+## To use -> replace [Client-ID] with your client ID and gameName with whatever game you want.
 
-## Parameters to use when your Query the API
-## Headers include your client ID / Oauth
-## Parameters include name of game or other strings as required by the API
+ClientID = '[Client-ID]'
+gameName = "World of Warcraft"
+channels = {}
+count = 0
 
 headers = {
-    'Client-ID': '[Client-ID]',
+    'Client-ID': ClientID,
 }
 params = (
-    ('name', "World of Warcraft"),
+    ('name', gameName),
 )
-channels = {}
-
-## Gets the ID of the game you're looking for by the title of the game
-## Sends a API request to twitch to get the game ID by passing in game name as a parameter
-response = requests.get('https://api.twitch.tv/helix/games', headers=headers, params=params)
-games = json.loads(response.text)
-print("Game Title: ", (games['data'][0]['name']))
-gameID = games['data'][0]['id']
 
 
-## Lists all of the channels in a directory (first loop to get the cursor to use for pagination)
-## Gets a list of the first 100 streamers in a category then returns a cursor, which you use as a parameter in following
-## requests to get the next 100 results, until there are no results remaining
-response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=(('game_id', gameID),('first', 100),))
-streamData = json.loads(response.text)
-cursor = streamData['pagination']['cursor']
-streamersList = streamData['data']
-for streamer in streamersList:
-    channels[streamer['user_name']] = streamer['title']
+## Gets the ID of the game you're looking for by the title of the game (ID needed for the API)
+## Give the function a gameName and it will return a gameID
+def getGameID(gameName):
+    response = requests.get('https://api.twitch.tv/helix/games', headers=headers, params=(('name', gameName),))
+    games = json.loads(response.text)
+    print("Game Title: ", (games['data'][0]['name']))
+    gameID = games['data'][0]['id']
+    return gameID
 
 
-## Loops through to get all of the channels (using pagination) until no channels remain
-while (cursor != -1):
-    response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=(('game_id', gameID),('first', 100), ('after', cursor),))
+## Lists all of the livestreams in a certain category.
+## Give the function a gameID and it will return a list of all live channels and their titles.
+def getListofStreams(gameID):
+    response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=(('game_id', gameID),('first', 100),))
     streamData = json.loads(response.text)
-    if (len(streamData['pagination']) > 0):
-         cursor = streamData['pagination']['cursor']
-    else:
-        cursor = -1
+    cursor = streamData['pagination']['cursor']
     streamersList = streamData['data']
     for streamer in streamersList:
-        channels[streamer['user_name']] = streamer['title']
+        channels[streamer['user_name']] = streamer['title'].strip()
 
-## Prints the usernames of all live channels in the category
+    ## Loops through to get all of the channels (using pagination) until no channels remain
+    while (cursor != -1):
+        response = requests.get('https://api.twitch.tv/helix/streams', headers=headers, params=(('game_id', gameID),('first', 100), ('after', cursor),))
+        streamData = json.loads(response.text)
+        if (len(streamData['pagination']) > 0):
+            cursor = streamData['pagination']['cursor']
+        else:
+            cursor = -1
+        streamersList = streamData['data']
+        for streamer in streamersList:
+         channels[streamer['user_name']] = streamer['title'].strip()
+    return channels
+
+
+## Function Calls
+gameID = getGameID(gameName)
+channels = getListofStreams(gameID)
+
+## Prints the list of channels
 for x in channels:
     print(x)
-
-print(len(channels), "channels")
